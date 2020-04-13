180b2a65cf2459eebe448cefee196baaf64adeb6
sualko <klaus@jsxc.org>
2020-04-13 11:36:48 +0200

    feat: add mockup elements to app bar

--- imports/ui/Panel.tsx
+++ imports/ui/Panel.tsx
@@ -1,32 +1,30 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import Drawer from '@material-ui/core/Drawer';
+import Badge from '@material-ui/core/Badge';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import Link from '@material-ui/core/Link';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import HelpIcon from '@material-ui/icons/Help';
 import MenuIcon from '@material-ui/icons/Menu';
-import IconButton from '@material-ui/core/IconButton';
-// import AreaList from '../containers/AreaList'
-import {
-    useRouteMatch,
-    Switch,
-    Route,
-    Link as RouterLink,
-    useParams,
-  } from "react-router-dom";
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import SearchIcon from '@material-ui/icons/Search';
+import clsx from 'clsx';
+import React from 'react';
+import { Link as RouterLink, Route, Switch, useRouteMatch } from "react-router-dom";
 import Area from './Area';
-import AreaList from './AreaList';
 import AreaLinkList from './AreaLinkList';
+import AreaList from './AreaList';
 import EditDevice from './EditDevice';
 import NewDevice from './NewDevice';
 
@@ -113,6 +111,44 @@ const useStyles = makeStyles(theme => ({
             width: theme.spacing(7),
         },
     },
+    grow: {
+        flexGrow: 1,
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 200,
+        },
+    },
 }));
 
 export default function Panel() {
@@ -125,6 +161,8 @@ export default function Panel() {
     const handleDrawerClose = () => {
         setOpen(false);
     };
+
+    const menuId = 'primary-search-account-menu';
 
     return (
         <div className={classes.root}>
@@ -143,6 +181,38 @@ export default function Panel() {
                     <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
                         Streetlight Control
                     </Typography>
+
+                    <div className={classes.grow} />
+
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'Search' }}
+                        />
+                    </div>
+
+                    <IconButton aria-label="Show 17 new notifications" color="inherit">
+                        <Badge badgeContent={17} color="secondary">
+                            <NotificationsIcon />
+                        </Badge>
+                    </IconButton>
+                    <IconButton
+                        edge="end"
+                        aria-label="Account of current user"
+                        aria-controls={menuId}
+                        aria-haspopup="true"
+                        // onClick={handleProfileMenuOpen}
+                        color="inherit"
+                    >
+                        <AccountCircle />
+                    </IconButton>
                 </Toolbar>
             </AppBar>
             <Drawer
