6d754a7f562b0c428688d994ac8beb6ca7bc8363
sualko <klaus@jsxc.org>
2020-04-14 16:48:41 +0200

    fix: show notice if no device was registered

--- imports/ui/DeviceList.tsx
+++ imports/ui/DeviceList.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Device from './Device';
+import { Paper, Typography } from '@material-ui/core';
 
 const DeviceList = ({ devices }) => {
     return (
         <div>
+            {devices.length === 0 && <Typography variant="body2">Noch keine Geräte vorhanden.</Typography>}
             {devices.map(device => (
                 <Device key={device._id} device={device} />
             ))}
