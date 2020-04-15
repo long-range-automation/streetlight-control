09b8e7158008d241848d38e64dc17d5ff8b934af
sualko <klaus@jsxc.org>
2020-04-15 09:21:11 +0200

    feat: prepare translation

--- client/main.tsx
+++ client/main.tsx
@@ -4,8 +4,26 @@ import { render } from 'react-dom';
 import { App } from '/imports/ui/App'
 import 'leaflet/dist/leaflet.css';
 import Leaflet from 'leaflet';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 Leaflet.Icon.Default.imagePath = '/leaflet_images/';
+
+i18n.use(LanguageDetector).use(initReactI18next).init({
+  fallbackLng: 'en',
+  debug: true,
+  resources: {
+    en: {
+      translation: {
+
+      }
+    }
+  },
+  interpolation: {
+    escapeValue: false, // not needed for react as it escapes by default
+  }
+});
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('react-target'));
